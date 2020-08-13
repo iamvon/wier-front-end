a1175d43c7815688f848cd42df1408a654c88654
blurk <nsinh6745@gmail.com>
2020-08-13 09:33:32 +0700

    ✔ Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Login } from "./scenes/Sign/scenes";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return <Login />;
