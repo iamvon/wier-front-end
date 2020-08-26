67e70a8696e2cd095e4a2f9300cd9dc37cb9f5be
blurk <nsinh6745@gmail.com>
2020-08-26 11:37:48 +0700

    🔨 Update changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from "react";
-import { ProfileView } from "./scenes/Profile/scenes";
-import { Login, Register } from "./scenes/Sign/scenes";
-import { Copyright, Header } from "./scenes/components";
-import { Item } from "./scenes/Home/components";
-import { HomePage } from "./scenes/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
-<<<<<<< HEAD
+import React from 'react';
+import { ProfileView } from './scenes/Profile/scenes';
+import { Login, Register } from './scenes/Sign/scenes';
+import { Copyright, Header } from './scenes/components';
+import { Item } from './scenes/Home/components';
+import { HomePage } from './scenes/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 
-=======
-import { Checkout } from "./scenes/Checkout/components";
-import {ProductDetail} from './scenes/ProductDetail/index'
->>>>>>> cbfaf6db81f9aea149180986823bd45b7c5cd2b9
+import { Checkout } from './scenes/Checkout/components';
+import { ProductDetail } from './scenes/ProductDetail/index';
 function App() {
   /* EXAMPLE VIEW FOR CODING */
   return (
@@ -23,25 +20,22 @@ function App() {
         <Route path="/login">
           <Login />
         </Route>
-          <Route path="/checkout">
-              <Checkout />
-          </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
         <Route path="/register">
           <Register />
         </Route>
         <Route path="/profile">
           <ProfileView />
         </Route>
-<<<<<<< HEAD
         <Route path="/"></Route>
-=======
-          <Route path="/productdetail">
+        <Route path="/productdetail">
           <ProductDetail />
         </Route>
         <Route exact path="/">
           <HomePage />
         </Route>
->>>>>>> cbfaf6db81f9aea149180986823bd45b7c5cd2b9
       </Switch>
       {/* COPYRIGHT FOOTER */}
       <Copyright />
--- src/scenes/components/Header/index.js
+++ src/scenes/components/Header/index.js
@@ -1,40 +1,19 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box,Grid, Typography } from "@material-ui/core";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Grid, Typography } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
 function Header() {
   const classes = useStyles();
 
   return (
-<<<<<<< HEAD
-    <Box className={classes.container}>
-      <Box flexGrow={1}>
-        <Typography>Wier</Typography>
-      </Box>
-      <Box flexGrow={1} display="flex" justifyContent="flex-end">
-        <NavLink className={classes.link} to="/">
-          Home
-        </NavLink>
-        <NavLink className={classes.link} to="/login">
-          Login
-        </NavLink>
-        <NavLink className={classes.link} to="/register">
-          Register
-        </NavLink>
-        <NavLink className={classes.link} to="/profile">
-          Profile
-        </NavLink>
-      </Box>
-    </Box>
-=======
     <div className={classes.root}>
       <div className={classes.container}>
         <Grid container>
           <Grid item xs>
-            <Box flexGrow={1} display="flex" >
+            <Box flexGrow={1} display="flex">
               <NavLink className={classes.link} to="/">
-              WIER
-            </NavLink>
+                WIER
+              </NavLink>
             </Box>
           </Grid>
           <Grid item xs>
@@ -51,12 +30,12 @@ function Header() {
               <NavLink className={classes.link} to="/profile">
                 Profile
               </NavLink>
-                <NavLink className={classes.link} to="/productdetail">
-                    Product detail
-                </NavLink>
-                <NavLink className={classes.link} to="/checkout">
-                    Checkout
-                </NavLink>
+              <NavLink className={classes.link} to="/productdetail">
+                Product detail
+              </NavLink>
+              <NavLink className={classes.link} to="/checkout">
+                Checkout
+              </NavLink>
             </Box>
           </Grid>
         </Grid>
@@ -73,7 +52,6 @@ function Header() {
         </Grid>
       </Grid> */}
     </div>
->>>>>>> cbfaf6db81f9aea149180986823bd45b7c5cd2b9
   );
 }
 
@@ -83,14 +61,14 @@ const useStyles = makeStyles((theme) => ({
   container: {
     flexGrow: 1,
     padding: theme.spacing(2),
-    display: "flex",
-    alignItems: "center",
-    backgroundColor: "#8092ed"
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#8092ed',
   },
   link: {
-    fontFamily: "Arial",
-    textDecoration: "none",
-    color: "fcfcfc",
+    fontFamily: 'Arial',
+    textDecoration: 'none',
+    color: 'fcfcfc',
     fontWeight: 500,
     padding: theme.spacing(1, 2),
   },
